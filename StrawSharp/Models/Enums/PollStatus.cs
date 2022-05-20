a6a144711b731fc008428143b6d5c6aedc791506
AppeazeTheCheese <yolosolodbaz@gmail.com>
2022-05-20 01:34:18 -0600

    Added EnumMember attributes to PollStatus enum members.

--- StrawSharp/Models/Enums/PollStatus.cs
+++ StrawSharp/Models/Enums/PollStatus.cs
@@ -1,12 +1,16 @@
-﻿using System.Text.Json.Serialization;
+﻿using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
 
 namespace StrawSharp.Models.Enums
 {
     [JsonStringEnumMemberConverterOptions(deserializationFailureFallbackValue: Published)]
     public enum PollStatus
     {
+        [EnumMember(Value = "draft")]
         Draft,
+        [EnumMember(Value = "published")]
         Published,
+        [EnumMember(Value = "blocked")]
         Blocked
     }
 }
